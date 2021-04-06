e7bb89be6ac8c7aa1edab653dc827ceaf5e46882
Arne Emmel <x-git@arnee.de>
2021-04-06 10:10:04 +0200

    zsh: Plugins as submodules

---
+++ zsh/plugins.zsh
@@ -0,0 +1,16 @@
+# Use syntax highlighting
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Use history substring search
+source ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+# bind UP and DOWN arrow keys to history substring search
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# Use autosuggestion
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=14'
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,5 +3,6 @@
 
 SRCDIR=$(dirname $(readlink -m "$0"))
 
+git submodule init && git submodule update
 ln -snfv $SRCDIR $HOME/.zsh
 ln -sfv $SRCDIR/zshrc $HOME/.zshrc
